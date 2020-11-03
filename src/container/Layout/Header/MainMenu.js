0bfc7422bd04146137ee79684213afe8a3e27010
Andres vasquez <you@example.com>
2020-11-03 18:05:03 -0500

    initial config

---
+++ src/container/Layout/Header/MainMenu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Menu } from 'antd';
+
+import {
+  HOME_PAGE,
+  LISTING_POSTS_PAGE,
+  AGENT_PROFILE_PAGE,
+  PRICING_PLAN_PAGE,
+} from 'settings/constant';
+
+const MainMenu = ({ className }) => {
+  return (
+    <Menu className={className}>
+      <Menu.Item key="0">
+        <NavLink exact to={`${HOME_PAGE}`} className="menu-items">
+          Inicio
+        </NavLink>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <NavLink to={`${LISTING_POSTS_PAGE}`}>Quiero ser host</NavLink>
+      </Menu.Item>
+      {/* <Menu.Item key="2">
+        <NavLink to={`${AGENT_PROFILE_PAGE}`}>Agent</NavLink>
+      </Menu.Item> */}
+      <Menu.Item key="3">
+        <NavLink to={`${PRICING_PLAN_PAGE}`}>Precios</NavLink>
+      </Menu.Item>
+    </Menu>
+  );
+};
+
+export default MainMenu;
