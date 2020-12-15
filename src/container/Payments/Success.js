01cf92e323f7b535cccf3219a18b090d181b7aa6
Andres vasquez <you@example.com>
2020-12-15 10:59:10 -0500

    configurations enviaroments

--- src/container/Payments/Success.js
+++ src/container/Payments/Success.js
@@ -3,10 +3,17 @@ import Loader from 'components/Loader/Loader';
 import { Result, Button } from 'antd';
 import { firestore } from '../../firebaseConfig';
 import {MY_TRIPS} from '../../settings/constant';
+import moment from 'moment';
+import 'moment/locale/es'  // without this line it didn't work
+
+moment.locale('es')
+
+
 export default function Success({history}) {
   const  [payment, setPayment] = useState(null)
   const [loading, setLoading] = useState(true)
-
+  var date = moment(new Date()).format('LLLL');
+  console.log(process.env)
   useEffect(() => {
     if(localStorage.getItem("last_payment")){
       setPayment(localStorage.getItem("last_payment"))
@@ -20,6 +27,7 @@ export default function Success({history}) {
     if(payment){
       firestore.collection("contract_houst").doc(payment).set({status: "APPROVED", tripStatus: "ACTIVE"}, {merge: true})
     }
+
   },[payment]);
 
 
@@ -37,7 +45,7 @@ export default function Success({history}) {
     (<Result
       status="success"
       title="Su Pago ha sido exitoso!"
-      subTitle={`Pago ID : ${payment} - ${new Date()}`}
+      subTitle={`Pago ID : ${payment} - ${date}`}
       extra={[
         <Button type="primary" key="console" onClick={goHostDetail} style={{backgroundColor: '#ffcf2c', borderColor: '#ffcf2c'}}>
           Ver Host
